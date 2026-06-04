e57a741e85eefd69377018bd3b51bc7f15280816
Sushant Ramkrushna Telgote <sushanttelgote4@gmail.com>
2026-06-04 17:55:19 +0530

    Update API endpoint for recipe submission

--- foodRecipe/frontend/food-blog-app/src/pages/AddFoodRecipe.jsx
+++ foodRecipe/frontend/food-blog-app/src/pages/AddFoodRecipe.jsx
@@ -12,7 +12,7 @@ export default function AddFoodRecipe() {
     const onHandleSubmit = async (e) => {
         e.preventDefault()
         console.log(recipeData)
-        await axios.post("http://localhost:5000/recipe", recipeData,{
+        await axios.post("https://food-recipe-api-gd0x.onrender.com/recipe", recipeData,{
             headers:{
                 'Content-Type':'multipart/form-data',
                 'authorization':'bearer '+localStorage.getItem("token")
